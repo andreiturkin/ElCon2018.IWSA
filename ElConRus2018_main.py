21b590723806a5a6fa4eddaec0cff5598583a68c
TAndrey <turkin.andrey.v@gmail.com>
2017-12-12 18:18:31 +0300

    Fixing main

--- ElConRus2018_main.py
+++ ElConRus2018_main.py
@@ -3,7 +3,7 @@ import timeit
 import datetime
 import matplotlib
 # http://matplotlib.org/faq/usage_faq.html#what-is-a-backend
-matplotlib.use('TkAgg')
+matplotlib.use('Agg')
 import progressbar
 from terminaltables import SingleTable
 
@@ -61,49 +61,49 @@ if __name__ == '__main__':
     print'#############################################################################\n'
     print'                              Figure 1a                                        '
     # Get the workspace
-    GetSolution_woS(0.05, rhomin = 8.0, rhomax = 12.0, \
+    GetSolution_woS(0.2, rhomin = 8.0, rhomax = 12.0, \
                          thetamin = pi/16.0, thetamax = pi - pi/16.0, \
                          fig = '1a', ShowRes=True)
     print'                              Figure 1b                                        '
     # Get the workspace
-    GetSolution_woS(0.05, rhomin = 8.0, rhomax = 12.0, \
+    GetSolution_woS(0.2, rhomin = 8.0, rhomax = 12.0, \
                           thetamin = 3*pi/4.0, thetamax = 2*pi-pi/4.0, \
                           fig = '1b', ShowRes=True)
     print'                              Figure 1c                                        '
     # Get the workspace
-    GetSolution_woS(0.05, rhomin = 8.0, rhomax = 12.0, \
+    GetSolution_woS(0.2, rhomin = 8.0, rhomax = 12.0, \
                           thetamin = pi + pi/16.0, thetamax = 2*pi - pi/16.0, \
                           fig = '1c', ShowRes=True)
     print'                              Figure 1d                                        '
     # Get the workspace
-    GetSolution_woS(0.05, rhomin = 8.0, rhomax = 12.0, \
+    GetSolution_woS(0.2, rhomin = 8.0, rhomax = 12.0, \
                           thetamin = 0.0, thetamax = 2*pi, \
                           fig = '1d', ShowRes=True)
     print'#############################################################################\n'
     print'                              Figure 3a                                        '
     # Get the workspace
-    GetSolution_wS(0.05, rhomin = 8.0, rhomax = 12.0, \
+    GetSolution_wS(0.2, rhomin = 8.0, rhomax = 12.0, \
                          thetamin = pi/16.0, thetamax = pi - pi/16.0, \
                          fig = '3a', ShowRes=True)
     print'                              Figure 3b                                        '
     # Get the workspace
-    GetSolution_wS(0.05, rhomin = 8.0, rhomax = 12.0, \
+    GetSolution_wS(0.2, rhomin = 8.0, rhomax = 12.0, \
                           thetamin = 3*pi/4.0, thetamax = 2*pi-pi/4.0, \
                           fig = '3b', ShowRes=True)
     print'                              Figure 3c                                        '
     # Get the workspace
-    GetSolution_wS(0.05, rhomin = 8.0, rhomax = 12.0, \
+    GetSolution_wS(0.2, rhomin = 8.0, rhomax = 12.0, \
                           thetamin = pi + pi/16.0, thetamax = 2*pi - pi/16.0, \
                           fig = '3c', ShowRes=True)
     print'                              Figure 3d                                        '
     # Get the workspace
-    GetSolution_wS(0.05, rhomin = 8.0, rhomax = 12.0, \
+    GetSolution_wS(0.2, rhomin = 8.0, rhomax = 12.0, \
                           thetamin = 0.0, thetamax = 2*pi, \
                           fig = '3d', ShowRes=True)
     print'###############################################################################'
     print'                             Table 1'
-    Table1_deltas = [0.5, 0.3, 0.25, 0.2, 0.14, 0.08, 0.07, 0.05, 0.03]
-    pbar = progressbar.ProgressBar(maxval=len(Example2_deltas), \
+    Table1_deltas = [0.5, 0.25, 0.2, 0.14, 0.07, 0.05, 0.03]
+    pbar = progressbar.ProgressBar(maxval=len(Table1_deltas), \
                                   widgets=[progressbar.Bar('=', '[', ']'), ' ', progressbar.Percentage()])
     pbar.start()
     rows = []
